676c801f491591df2b7ffc59912f3b0880e3a40f
AdiZeljkovic <adi.zeljkovic@outlook.com>
2025-12-27 21:29:22 +0100

    Manually attach XSRF-TOKEN header to requests

--- frontend-next/src/lib/api.ts
+++ frontend-next/src/lib/api.ts
@@ -15,6 +15,25 @@ export const getCsrfToken = async () => {
     await axios.get(`${rootUrl}/sanctum/csrf-cookie`, { withCredentials: true });
 };
 
+// Helper to getting cookie value
+function getCookie(name: string) {
+    if (typeof document === 'undefined') return null;
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop()?.split(';').shift();
+    return null;
+}
+
+// Request Interceptor
+api.interceptors.request.use((config) => {
+    // Manually attach X-XSRF-TOKEN from cookie if present
+    const token = getCookie('XSRF-TOKEN');
+    if (token) {
+        config.headers['X-XSRF-TOKEN'] = decodeURIComponent(token);
+    }
+    return config;
+});
+
 // Response Interceptor
 api.interceptors.response.use(
     (response) => response,
